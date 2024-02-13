c9547f3099ec0cb4ad8b19550e22cec28d70bd6c
Jey <126204426+Jeyrama@users.noreply.github.com>
2024-02-12 18:30:29 -0800

    Update Almost-even.js

--- Almost-even.js
+++ Almost-even.js
@@ -40,3 +40,13 @@ function splitInteger(num, parts) {
 }
 
 // or
+
+function splitInteger(num, parts) {
+  const remainder = num % parts;
+  const int = Math.floor(num / parts);
+
+  return [
+    ...new Array(parts - remainder).fill(int),
+    ...new Array(remainder).fill(int + 1)
+  ];
+}
